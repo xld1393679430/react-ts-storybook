215006e6fa3e1675f795676ea4fc3eb7286b86bc
ld_xu <ld_xu@trip.com>
2022-10-23 12:06:12 +0800

    为SubMenu增加useCall,ueMemo和meo提升性能

--- src/components/Menu/SubMenu/index.tsx
+++ src/components/Menu/SubMenu/index.tsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { memo, useCallback, useContext, useMemo, useRef, useState } from "react";
 import classNames from "classnames";
 import { MenuContext } from "../index";
 import { MenuItemProps } from "../MenuItem";
+import Icon from "../../Icon";
 
 export interface BaseSubMenuProps {
   index?: string;
@@ -11,49 +12,56 @@ export interface BaseSubMenuProps {
 
 export type SubMenuProps = BaseSubMenuProps & React.HTMLAttributes<HTMLElement>;
 
-const Index: React.FC<SubMenuProps> = ({ index, title, className, children, ...rest }) => {
+const Index: React.FC<SubMenuProps> = memo(({ index, title, className, children, ...rest }) => {
   const context = useContext(MenuContext);
   const openedSubMenus = context.defaultOpenSubMenus as Array<string>;
   const isOpen = index && context.mode === "vertical" ? openedSubMenus.includes(index) : false;
   const [menuOpen, setMenuOpen] = useState(isOpen);
-  const classes = classNames("menu-item submenu-item", className, {
-    "is-active": context.index?.startsWith(index || ""),
-  });
+  const classes = useMemo(() => {
+    return classNames("menu-item submenu-item", className, {
+      "is-active": context.index?.startsWith(index || ""),
+      "is-opened": menuOpen,
+      "is-vertical": context.mode === "vertical",
+    });
+  }, [className, context.index, context.mode, index, menuOpen]);
 
-  let timer: any;
-  const handleMouse = (e: React.MouseEvent, toggle: boolean) => {
-    timer && clearTimeout(timer);
+  let timer: any = useRef();
+  const handleMouse = useCallback((e: React.MouseEvent, toggle: boolean) => {
+    timer.current && clearTimeout(timer.current);
     e.preventDefault();
-    timer = setTimeout(() => {
+    timer.current = setTimeout(() => {
       setMenuOpen(toggle);
     }, 300);
-  };
+  }, []);
 
-  const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    // 点击的是SubMenu的MenuItem时不需要关闭
-    if (index?.indexOf("-") !== -1) {
+  const handleClick = useCallback(
+    (e: React.MouseEvent) => {
+      e.preventDefault();
       setMenuOpen(!menuOpen);
-    }
 
-    if (context.onSelect && typeof index === "string") {
-      context.onSelect(index);
-    }
-  };
+      if (context.onSelect && typeof index === "string") {
+        context.onSelect(index);
+      }
+    },
+    [context, index, menuOpen]
+  );
 
-  const clickEvents = {
-    onClick: handleClick,
-  };
+  const clickEvents = useMemo(() => {
+    return {
+      onClick: handleClick,
+    };
+  }, [handleClick]);
 
-  const hoverEvents =
-    context.mode === "vertical"
+  const hoverEvents = useMemo(() => {
+    return context.mode === "vertical"
       ? {}
       : {
           onMouseEnter: (e: React.MouseEvent) => handleMouse(e, true),
           onMouseLeave: (e: React.MouseEvent) => handleMouse(e, false),
         };
+  }, [context.mode, handleMouse]);
 
-  const renderChildren = () => {
+  const renderChildren = useCallback(() => {
     const subMenuClasses = classNames("viking-submenu", {
       "menu-opened": menuOpen,
     });
@@ -69,17 +77,18 @@ const Index: React.FC<SubMenuProps> = ({ index, title, className, children, ...r
       }
     });
     return <ul className={subMenuClasses}>{childrenComponent}</ul>;
-  };
+  }, [children, index, menuOpen]);
 
   return (
     <li key={index} className={classes} {...hoverEvents} {...rest}>
       <div className="submenu-title" {...clickEvents}>
         {title}
+        <Icon icon="angle-down" className="arrow-icon" />
       </div>
       {renderChildren()}
     </li>
   );
-};
+});
 
 Index.displayName = "SubMenu";
 
