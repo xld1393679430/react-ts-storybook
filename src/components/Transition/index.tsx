8d20e56330495c005efbe9d7b8f03e05c406b2fe
ld_xu <ld_xu@trip.com>
2022-10-23 22:57:25 +0800

    优化Transition组件

--- src/components/Transition/index.tsx
+++ src/components/Transition/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo, useCallback } from "react";
 import { CSSTransition } from "react-transition-group";
 import { CSSTransitionProps } from "react-transition-group/CSSTransition";
 
@@ -6,22 +6,32 @@ type AnimationName = "zoom-in-top" | "zoom-in-left" | "zoom-in-right" | "zoom-in
 
 type TransitionProps = CSSTransitionProps & {
   animation?: AnimationName;
+  wrapper?: boolean;
 };
 
-const Index: React.FC<TransitionProps> = (props) => {
-  const { children, classNames, animation, ...rest } = props;
+const Index: React.FC<TransitionProps> = memo((props) => {
+  const { children, classNames, animation, wrapper, ...rest } = props;
+
+  const renderChildrend = useCallback(() => {
+    // if (wrapper) {
+    //   return <div>{children as any}</div>;
+    // }
+
+    return children;
+  }, [children]);
 
   return (
     <CSSTransition classNames={classNames ? classNames : animation} {...rest}>
-      {children}
+      {renderChildrend()}
     </CSSTransition>
   );
-};
+});
 
 Index.displayName = "Transition";
 Index.defaultProps = {
   unmountOnExit: true,
   appear: true,
+  wrapper: false,
 };
 
 export default Index;
